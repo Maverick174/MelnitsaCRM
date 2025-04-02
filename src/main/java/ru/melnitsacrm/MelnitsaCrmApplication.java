5797e8e6b10545bbcec2b8de2cf46f78ddaf2ea7
Maverick <maverick174@mail.ru>
2025-04-02 19:48:35 +0500

    Initial commit

---
+++ src/main/java/ru/melnitsacrm/MelnitsaCrmApplication.java
@@ -0,0 +1,13 @@
+package ru.melnitsacrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelnitsaCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelnitsaCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/melnitsacrm/MelnitsaCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.melnitsacrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelnitsaCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
